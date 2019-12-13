fa7295a72ba20b18a4f7b36f141265dcc0d579b9
Rafael Willians de Sousa <rafaelwillians21.com>
2019-12-12 23:50:26 -0300

    Inclusão de mais componentes

--- src/app/layout/rodape/rodape.component.ts
+++ src/app/layout/rodape/rodape.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RodapeComponent implements OnInit {
 
+  listaLinks = [
+    {
+      descricao: 'Sobre nós',
+      links: [
+        {nome: 'Perfil', url: '#'},
+        {nome: 'Sobre', url: '#'},
+        {nome: 'Contato', url: '#'}
+      ]
+    },
+    {
+      descricao: 'Redes Sociais',
+      links: [
+        {nome: 'Twitter', url: '#'},
+        {nome: 'Instagram', url: '#'},
+        {nome: 'Linkedin', url: '#'}
+      ]
+    },
+    {
+      descricao: 'Contato',
+      links: [
+        {nome: 'FAC', url: '#'},
+        {nome: 'Sitemap', url: '#'},
+        {nome: 'Dúvidas', url: '#'}
+      ]
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/resrva-ingresso/reserva-ingresso.module.ts
+++ src/app/resrva-ingresso/reserva-ingresso.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './pages/form/form.component';
+import { EnderecoFormComponent } from './components/endereco-form/endereco-form.component';
 
 @NgModule({
-  declarations: [FormComponent],
+  declarations: [
+    FormComponent,
+    EnderecoFormComponent,
+  ],
   imports: [
     CommonModule
   ]
